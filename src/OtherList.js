6a6f3b2f36f8945252e1ef2ff758b2e6a92bd6db
katie-ar <148056793+katie-ar@users.noreply.github.com>
2023-10-19 11:10:35 +0100

    Update OtherList.js

--- src/OtherList.js
+++ src/OtherList.js
@@ -1,12 +1,12 @@
-import { useState, useEffect } from "react"
-import Button from "./Button"
+import { useState, useEffect } from "react";
+import Button from "./Button";
 
 // Displays the list of 'other' genres when the user clicks the 'See other' button and hides them when the user clicks the 'Hide other' button
 function OtherList({otherList}){
-    const [otherView, setOtherView] = useState(false)
+    const [otherView, setOtherView] = useState(false);
 
     const handleOtherClick = (value) => {
-        setOtherView(value)
+        setOtherView(value);
     }
     
     // The 'other' list is hidden by default
